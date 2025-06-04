2aa50d50707e781bf6ea9a5a06e0242d8fadc462
Casey Fan <fan.casey23@gmail.com>
2025-06-04 01:02:36 -0500

    update venv & finish team demo #2

--- autogen_demo/swarm_demo.py
+++ autogen_demo/swarm_demo.py
@@ -24,6 +24,13 @@ from autogen_core.tools import FunctionTool, Tool
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 from pydantic import BaseModel
 
+from dotenv import load_dotenv
+import os
+import asyncio
+
+load_dotenv()
+api_key: str = os.environ["API_KEY"]
+
 class UserLogin(BaseModel):
     pass
 
@@ -132,3 +139,257 @@ class AIAgent(RoutedAgent):
             AgentResponse(context=message.context, reply_to_topic_type=self._agent_topic_type),
             topic_id=TopicId(self._user_topic_type, source=self.id.key),
         )
+
+class HumanAgent(RoutedAgent):
+    def __init__(self, description: str, agent_topic_type: str, user_topic_type: str) -> None:
+        super().__init__(description)
+        self._agent_topic_type = agent_topic_type
+        self._user_topic_type = user_topic_type
+
+    @message_handler
+    async def handle_user_task(self, message: UserTask, ctx: MessageContext) -> None:
+        human_input = input("Human agent input: ")
+        print(f"{'-'*80}\n{self.id.type}:\n{human_input}", flush=True)
+        message.context.append(AssistantMessage(content=human_input, source=self.id.type))
+        await self.publish_message(
+            AgentResponse(context=message.context, reply_to_topic_type=self._agent_topic_type),
+            topic_id=TopicId(self._user_topic_type, source=self.id.key),
+        )
+
+class UserAgent(RoutedAgent):
+    def __init__(self, description: str, user_topic_type: str, agent_topic_type: str) -> None:
+        super().__init__(description)
+        self._user_topic_type = user_topic_type
+        self._agent_topic_type = agent_topic_type
+
+    @message_handler
+    async def handle_user_login(self, message: UserLogin, ctx: MessageContext) -> None:
+        print(f"{'-'*80}\nUser login, session ID: {self.id.key}.", flush=True)
+        # Get the user's initial input after login.
+        user_input = input("User: ")
+        print(f"{'-'*80}\n{self.id.type}:\n{user_input}")
+        await self.publish_message(
+            UserTask(context=[UserMessage(content=user_input, source="User")]),
+            topic_id=TopicId(self._agent_topic_type, source=self.id.key),
+        )
+
+    @message_handler
+    async def handle_task_result(self, message: AgentResponse, ctx: MessageContext) -> None:
+        # Get the user's input after receiving a response from an agent.
+        user_input = input("User (type 'exit' to close the session): ")
+        print(f"{'-'*80}\n{self.id.type}:\n{user_input}", flush=True)
+        if user_input.strip().lower() == "exit":
+            print(f"{'-'*80}\nUser session ended, session ID: {self.id.key}.")
+            return
+        message.context.append(UserMessage(content=user_input, source="User"))
+        await self.publish_message(
+            UserTask(context=message.context), topic_id=TopicId(message.reply_to_topic_type, source=self.id.key)
+        )
+
+# ===== TOOLS ======
+
+def execute_order(product: str, price: int) -> str:
+    print("\n\n=== Order Summary ===")
+    print(f"Product: {product}")
+    print(f"Price: ${price}")
+    print("=================\n")
+    confirm = input("Confirm order? y/n: ").strip().lower()
+    if confirm == "y":
+        print("Order execution successful!")
+        return "Success"
+    else:
+        print("Order cancelled!")
+        return "User cancelled order."
+
+
+def look_up_item(search_query: str) -> str:
+    item_id = "item_132612938"
+    print("Found item:", item_id)
+    return item_id
+
+
+def execute_refund(item_id: str, reason: str = "not provided") -> str:
+    print("\n\n=== Refund Summary ===")
+    print(f"Item ID: {item_id}")
+    print(f"Reason: {reason}")
+    print("=================\n")
+    print("Refund execution successful!")
+    return "success"
+
+
+execute_order_tool = FunctionTool(execute_order, description="Price should be in USD.")
+look_up_item_tool = FunctionTool(
+    look_up_item, description="Use to find item ID.\nSearch query can be a description or keywords."
+)
+execute_refund_tool = FunctionTool(execute_refund, description="")
+
+# ===== TOPICs =====
+sales_agent_topic_type = "SalesAgent"
+issues_and_repairs_agent_topic_type = "IssuesAndRepairsAgent"
+triage_agent_topic_type = "TriageAgent"
+human_agent_topic_type = "HumanAgent"
+user_topic_type = "User"
+
+# DELEGATE TOOLS
+
+def transfer_to_sales_agent() -> str:
+    return sales_agent_topic_type
+
+
+def transfer_to_issues_and_repairs() -> str:
+    return issues_and_repairs_agent_topic_type
+
+
+def transfer_back_to_triage() -> str:
+    return triage_agent_topic_type
+
+
+def escalate_to_human() -> str:
+    return human_agent_topic_type
+
+
+transfer_to_sales_agent_tool = FunctionTool(
+    transfer_to_sales_agent, description="Use for anything sales or buying related."
+)
+transfer_to_issues_and_repairs_tool = FunctionTool(
+    transfer_to_issues_and_repairs, description="Use for issues, repairs, or refunds."
+)
+transfer_back_to_triage_tool = FunctionTool(
+    transfer_back_to_triage,
+    description="Call this if the user brings up a topic outside of your purview,\nincluding escalating to human.",
+)
+escalate_to_human_tool = FunctionTool(escalate_to_human, description="Only call this if explicitly asked to.")
+
+# Creating the team
+
+async def create_and_run_team():
+    runtime = SingleThreadedAgentRuntime()
+
+    model_client = OpenAIChatCompletionClient(
+        model="gpt-4o-mini",
+        api_key=api_key,
+    )
+
+    # Register the triage agent.
+    triage_agent_type = await AIAgent.register(
+        runtime,
+        type=triage_agent_topic_type,  # Using the topic type as the agent type.
+        factory=lambda: AIAgent(
+            description="A triage agent.",
+            system_message=SystemMessage(
+                content="You are a customer service bot for ACME Inc. "
+                "Introduce yourself. Always be very brief. "
+                "Gather information to direct the customer to the right department. "
+                "But make your questions subtle and natural."
+            ),
+            model_client=model_client,
+            tools=[],
+            delegate_tools=[
+                transfer_to_issues_and_repairs_tool,
+                transfer_to_sales_agent_tool,
+                escalate_to_human_tool,
+            ],
+            agent_topic_type=triage_agent_topic_type,
+            user_topic_type=user_topic_type,
+        ),
+    )
+    # Add subscriptions for the triage agent: it will receive messages published to its own topic only.
+    await runtime.add_subscription(TypeSubscription(topic_type=triage_agent_topic_type, agent_type=triage_agent_type.type))
+
+    # Register the sales agent.
+    sales_agent_type = await AIAgent.register(
+        runtime,
+        type=sales_agent_topic_type,  # Using the topic type as the agent type.
+        factory=lambda: AIAgent(
+            description="A sales agent.",
+            system_message=SystemMessage(
+                content="You are a sales agent for ACME Inc."
+                "Always answer in a sentence or less."
+                "Follow the following routine with the user:"
+                "1. Ask them about any problems in their life related to catching roadrunners.\n"
+                "2. Casually mention one of ACME's crazy made-up products can help.\n"
+                " - Don't mention price.\n"
+                "3. Once the user is bought in, drop a ridiculous price.\n"
+                "4. Only after everything, and if the user says yes, "
+                "tell them a crazy caveat and execute their order.\n"
+                ""
+            ),
+            model_client=model_client,
+            tools=[execute_order_tool],
+            delegate_tools=[transfer_back_to_triage_tool],
+            agent_topic_type=sales_agent_topic_type,
+            user_topic_type=user_topic_type,
+        ),
+    )
+    # Add subscriptions for the sales agent: it will receive messages published to its own topic only.
+    await runtime.add_subscription(TypeSubscription(topic_type=sales_agent_topic_type, agent_type=sales_agent_type.type))
+
+    # Register the issues and repairs agent.
+    issues_and_repairs_agent_type = await AIAgent.register(
+        runtime,
+        type=issues_and_repairs_agent_topic_type,  # Using the topic type as the agent type.
+        factory=lambda: AIAgent(
+            description="An issues and repairs agent.",
+            system_message=SystemMessage(
+                content="You are a customer support agent for ACME Inc."
+                "Always answer in a sentence or less."
+                "Follow the following routine with the user:"
+                "1. First, ask probing questions and understand the user's problem deeper.\n"
+                " - unless the user has already provided a reason.\n"
+                "2. Propose a fix (make one up).\n"
+                "3. ONLY if not satisfied, offer a refund.\n"
+                "4. If accepted, search for the ID and then execute refund."
+            ),
+            model_client=model_client,
+            tools=[
+                execute_refund_tool,
+                look_up_item_tool,
+            ],
+            delegate_tools=[transfer_back_to_triage_tool],
+            agent_topic_type=issues_and_repairs_agent_topic_type,
+            user_topic_type=user_topic_type,
+        ),
+    )
+    # Add subscriptions for the issues and repairs agent: it will receive messages published to its own topic only.
+    await runtime.add_subscription(
+        TypeSubscription(topic_type=issues_and_repairs_agent_topic_type, agent_type=issues_and_repairs_agent_type.type)
+    )
+
+    # Register the human agent.
+    human_agent_type = await HumanAgent.register(
+        runtime,
+        type=human_agent_topic_type,  # Using the topic type as the agent type.
+        factory=lambda: HumanAgent(
+            description="A human agent.",
+            agent_topic_type=human_agent_topic_type,
+            user_topic_type=user_topic_type,
+        ),
+    )
+    # Add subscriptions for the human agent: it will receive messages published to its own topic only.
+    await runtime.add_subscription(TypeSubscription(topic_type=human_agent_topic_type, agent_type=human_agent_type.type))
+
+    # Register the user agent.
+    user_agent_type = await UserAgent.register(
+        runtime,
+        type=user_topic_type,
+        factory=lambda: UserAgent(
+            description="A user agent.",
+            user_topic_type=user_topic_type,
+            agent_topic_type=triage_agent_topic_type,  # Start with the triage agent.
+        ),
+    )
+    # Add subscriptions for the user agent: it will receive messages published to its own topic only.
+    await runtime.add_subscription(TypeSubscription(topic_type=user_topic_type, agent_type=user_agent_type.type))
+
+    # Start the runtime.
+    runtime.start()
+
+    # Create a new session for the user.
+    session_id = str(uuid.uuid4())
+    await runtime.publish_message(UserLogin(), topic_id=TopicId(user_topic_type, source=session_id))
+
+    # Run until completion.
+    await runtime.stop_when_idle()
+    await model_client.close()
+
+asyncio.run(create_and_run_team())
